bd2007f8e302d2f97ab143379a93131f9452a480
rickyraihan <rickyraihan83@gmail.com>
2024-01-18 12:55:12 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,27 @@
-import './style.scss'
+import "./css/style2.css";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const interBubble = document.querySelector<HTMLDivElement>(".interactive")!;
+  let curX = 0;
+  let curY = 0;
+  let tgX = 0;
+  let tgY = 0;
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    function move() {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(() => {
-            move();
-        });
-    }
-
-    window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
+  function move() {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    interBubble.style.transform = `translate(${Math.round(
+      curX
+    )}px, ${Math.round(curY)}px)`;
+    requestAnimationFrame(() => {
+      move();
     });
+  }
 
-    move();
+  window.addEventListener("mousemove", (event) => {
+    tgX = event.clientX;
+    tgY = event.clientY;
+  });
+
+  move();
 });
-
-
